26554898323edf88e44f9c9268c0a0cb7266de32
dmitrybdrv <incubator-it@protonmail.com>
2022-10-08 15:06:50 +0300

    deploy test

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -6,28 +6,37 @@ import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
 /*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
 * 3 - в файле Message.tsx отобразить приходящие данные
 * 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
 * 5 - сделать стили в соответствии с дизайном
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number,
+    user: {
+        avatar?: string,
+        name: string,
+    },
+    message: {
+        text: string,
+        time: string,
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Brad Pitt',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
+        text:  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quia!', // можно менять
+        time: '23:59', // можно менять
     },
 }
+
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
@@ -41,8 +50,10 @@ export const friendMessage0: MessageType = {
 }
 
 const HW1 = () => {
+
     return (
         <div id={'hw1'}>
+
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
-// нужно создать правильный тип вместо any
-export type MessagePropsType = any
+
+export type MessagePropsType = {
+    message: MessageType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
+
             <div className={s.imageAndText}>
-                <img
-                    id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
+                <img id={'hw1-avatar-' + props.message.id} src={props.message.user.avatar}/>
 
-                    //
-                />
                 <div className={s.text}>
+
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        {props.message.user.name}
                     </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
 
-                        {/**/}
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                        
+                       <p>{props.message.message.text}</p>
+
                     </pre>
                 </div>
             </div>
@@ -33,6 +33,7 @@ const Message = (props: MessagePropsType) => {
 
                 {/**/}
             </div>
+
         </div>
     )
 }
